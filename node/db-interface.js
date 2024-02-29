dfaa34a6f772014bf75ddffa7ad8dc2e2082c7d9
Nev0 <nevenarcisista@gmail.com>
2024-02-29 06:56:48 -0300

    Mudancas na infraestrutura do projeto

--- node/db-interface.js
+++ node/db-interface.js
@@ -2,37 +2,27 @@ const mysql = require('mysql2/promise')
 const express = require('express')()
 require('dotenv').config()
 
-
 express.listen(3000, () => {
     console.log('CONNECTED ON PORT 3000')
 })
 
-function queryTest() {
-    const connection = mysql.createPool({
-        host     : 'mysql-db',
-        port     : '3306',
-        user     : 'root',
-        password : process.env.DB_PASSWORD,
-        database : 'projects',
-    });
-    
-
-    express.get('/', (req, res) => {
-        result = connection.query('SELECT * FROM libre').then((query) => {
-            res.send(query)
-            console.log(query)
-        })
-    })
-    
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-  
-sleep(0).then(() => { 
-    console.log('Node server is running')
-    queryTest() 
+const connection = mysql.createPool({
+    host     : 'mysql-db',
+    port     : '3306',
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : 'projects',
 });
+
+
+express.get('/', (req, res) => {
+    result = connection.query('SELECT * FROM libre').then((query) => {
+        res.send(query)
+    }).catch( ( err ) => {
+        res.sendStatus(503)
+        console.error(err)
+    })
+})   
+
   
 
